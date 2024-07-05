4f2cee09f2a0aa5c114d701b925c1ba5dd89a11d
ShymaaRagab <102189654+ShymaaRagab@users.noreply.github.com>
2024-07-06 01:57:51 +0300

    .

--- JavaScript/formValidation.js
+++ JavaScript/formValidation.js
@@ -3,39 +3,49 @@ let userName = document.getElementsByName("userName")[0];
 let email = document.querySelector("[name='email']");
 let message = document.querySelector("[name='message']");
 let checkForm = document.getElementById("checkForm");
-// /\w+\d+@\w{2,}.com/g
-contactForm.addEventListener("submit", (event) => {
-  let userValid = false;
-  let emailValid = false;
-  let messageValid = true;
-  if (userName.value !== "" && userName.value.length <= 20) {
-    userValid = true;
-  }
-  if (
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const data = Object.fromEntries(new FormData(e.target));
+  const checkUserName = data["userName"] != "" || data["userName"].length > 30;
+  const checkEmail =
     /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-      email.value
-    )
-  ) {
-    emailValid = true;
-  }
+      data["email"]
+    );
 
-  if (userValid == false || emailValid == false || messageValid == false) {
-    event.preventDefault();
-    if (userValid == false) {
-      userName.style.borderColor = "red";
-    } else {
-      userName.style.borderColor = "#000";
-    }
-    if (emailValid == false) {
-      email.style.borderColor = "red";
-    } else {
-      email.style.borderColor = "#000";
-    }
+  const checkMessage = data["message"] != "";
 
-    checkForm.innerHTML = "please fill All your filds";
-    checkForm.style.color = "red";
+  if (checkUserName) {
+    userName.style.border = "2px solid hsl(231, 7%, 60%)";
+    userName.style.backgroundColor = "transparent";
   } else {
-    checkForm.innerHTML = "your message sended";
-    checkForm.style.color = "#00dbc9";
+    userName.style.border = "2px solid hsl(4, 100%, 67%)";
+    userName.style.backgroundColor = "hsl(4, 100%, 67% , 0.1)";
   }
-});
+
+  if (checkEmail) {
+    email.style.border = "2px solid hsl(231, 7%, 60%)";
+    email.style.backgroundColor = "transparent";
+  } else {
+    email.style.border = "2px solid hsl(4, 100%, 67%)";
+    email.style.backgroundColor = "hsl(4, 100%, 67% , 0.1)";
+  }
+
+  if (checkMessage) {
+    message.style.border = "2px solid hsl(231, 7%, 60%)";
+    message.style.backgroundColor = "transparent";
+  } else {
+    message.style.border = "2px solid hsl(4, 100%, 67%)";
+    message.style.backgroundColor = "hsl(4, 100%, 67% , 0.1)";
+  }
+
+  if (checkEmail && checkMessage && checkUserName) {
+    checkForm.innerHTML = "Your feedback sent successfully";
+    checkForm.style.color = "#353535";
+  } else {
+    checkForm.innerHTML = "Please fill out the entire form";
+    checkForm.style.color = "hsl(4, 100%, 67%)";
+  }
+}
+
+contactForm.addEventListener("submit", handleSubmit);
